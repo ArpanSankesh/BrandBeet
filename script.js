c135f066d3beb714c8ff9b98fa050bfb19f9dd27
ArpanSankesh <arpansankesh830@gmail.com>
2024-02-11 22:39:04 +0530

    overlay animation

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ requestAnimationFrame(raf)
 
 var tl =  gsap.timeline({scrollTrigger:{
     trigger:".section-1",
-    start:"27% 50%",
+    start:"25% 50%",
     end:"150% 50%",
     scrub:true,
     markers:true,
@@ -21,22 +21,35 @@ var tl =  gsap.timeline({scrollTrigger:{
 }});
 
 tl.to('.rotate-container',{
-    rotate:-15,
-    scale:0.6
+    rotate:-20,
+    scale:0.8
 },'animate')
 
 tl.to('.row1',{
+    // marginTop: "-30vw"
     marginTop: "-30vw"
 },'animate')
 
 tl.to('.row2',{
-    marginTop: "-50vw"
+    // marginTop: "-50vw"
+    marginTop: "-30vw"
 },'animate')
 
 tl.to('.row3',{
-    marginTop: "-70vw"
+    // marginTop: "-70vw"
+    marginTop: "-50vw"
 },'animate')
 
 tl.to('.row4',{
-    marginTop: "-80vw"
+    // marginTop: "-80vw"
+    marginTop: "-65vw"
+},'animate')
+
+tl.to('.overlay',{
+    // marginTop: "-80vw"
+    backgroundColor:"#000000c0"
+},'animate')
+
+tl.to('.overlay h1',{
+    opacity:1
 },'animate')
